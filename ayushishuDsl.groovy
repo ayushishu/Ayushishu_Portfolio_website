7be60ef94dd3551af7b2aa7c600ed45f0e5cafd3
“[Ayush]” <[ayushayush.sadwal@gmail.com]>
2022-09-24 04:41:49 +0530

    modified dsl file

--- ayushishuDsl.groovy
+++ ayushishuDsl.groovy
@@ -1,7 +1,7 @@
 job('who-is-ayush-wedsite') {
     description("CI-CD pipe-line for Ayushishu portfolio website ")
     
-    scm{
+    scm {
         git("https://github.com/ayushishu/Ayushishu_Portfolio_website/", dev)
     }
     triggers {
